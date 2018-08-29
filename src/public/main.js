7b9551bc7ee08313830c05f2db00c98705f0e613
nadavatar <nadavatar64@gmail.com>
2018-08-29 22:40:45 +0300

    edited stickynote

--- src/public/main.js
+++ src/public/main.js
@@ -61,16 +61,45 @@ function getStickyNotesRows() {
     const stickyNote = stickyNotes[index];
         
         html += `
-            <tr>
+            <tr id="stickyNote-${stickyNote.id}">
                 <td>${stickyNote.id}</td>
-                <td>${stickyNote.content}</td>
-                <td><i class="glyphicon glyphicon-trash" onclick="deleteStickyNote(${stickyNote.id})"></i></td>
+                <td>
+                    <span class="sticky-content non-edit-element">${stickyNote.content}</span>
+                    <input class="sticky-content-input form-control edit-element" value="${stickyNote.content}" />
+                </td>
+                <td>
+                    <i class="clickable glyphicon glyphicon-check edit-element" onclick="updateStickyNote(${stickyNote.id})"></i>
+                    <i class="clickable glyphicon glyphicon-pencil sticky-pencil non-edit-element" onclick="toggleEditMode(${stickyNote.id})"></i>
+                    <i class="clickable glyphicon glyphicon-trash" onclick="deleteStickyNote(${stickyNote.id})"></i>
+                    
+                </td>
             </tr>
         `;
         
     }
 
     return html;
+}
+
+function toggleEditMode(stickyNoteId) {
+    // Toggle edit mode
+    const stickyNoteRow = document.getElementById(`stickyNote-${stickyNoteId}`);
+    stickyNoteRow.classList.add('edit-mode');
+}
+
+function updateStickyNote(stickyNoteId) {
+    const stickyNoteContent = document.querySelector(`#stickyNote-${stickyNoteId} .sticky-content-input`);
+    for (let i = 0; i < stickyNotes.length; i++) {
+        const stickyNote = stickyNotes[i];
+        if (stickyNote.id === stickyNoteId) {
+            stickyNote.content = stickyNoteContent.value;
+            break;
+        }
+
+    }
+    setData('stickyNotes', stickyNotes);
+    renderList();
+
 }
 
 function deleteStickyNote(stickNoteId) {
@@ -94,5 +123,5 @@ function getStickyNotes() {
 }
 
 function setData(dataName, dataValue) {
-    localStorage.setItem(dataName, JSON.stringify(value));
+    localStorage.setItem(dataName, JSON.stringify(dataValue));
 }
