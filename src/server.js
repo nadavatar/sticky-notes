5250150685f759cb070e40eecf1de0cfa7a688dc
nadavatar <nadavatar64@gmail.com>
2019-07-06 18:37:00 +0300

    Adding MongoDB Compatibility

--- src/server.js
+++ src/server.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
 const app = express();
 
-const stickyNotes = [
-    {
-        id: Date.now(),
-        content: 'Do Something'
+let stickyNotes = [];
 
-    },
-    {
-        id: Date.now() + 10,
-        content: 'Do Something else'
+//mongodb get all sticky notes
+const mongoUrl = "mongodb://localhost:27017";
+mongo.connect(mongoUrl, { useNewUrlParser: true }, async function(
+  err,
+  connection
+) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Succesfully connected to the database");
+    const db = connection.db("stickyNotes");
+    const stickyNotesCollection = db.collection("stickyNotes");
+    stickyNotes = await stickyNotesCollection.find({}).toArray();
+  }
+  connection.close();
+});
 
-    }
-];
+console.log(stickyNotes);
 
-app.use(express.static('./src/public'))
+app.use(express.static("./src/public"));
 
-app.get('/sticky-notes', (req, res) => {
-    console.log('Got a request for sticky notes');
-    res.json(stickyNotes);
+app.get("/sticky-notes", (req, res) => {
+  console.log("Got a request for sticky notes");
+  res.json(stickyNotes);
 });
 
 const port = 3000;
 app.listen(port, () => {
-   console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
